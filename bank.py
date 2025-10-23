da8a099fd3d92bbe5e1e9ba7d14531e691185615
chaitanya <chaitanyakumbar30@gmail.com>
2025-10-23 15:32:34 +0530

    save

--- bank.py
+++ bank.py
@@ -6,3 +6,8 @@ new_balance = initial_balance + deposit
 print(f"initial balance:{initial_balance}")
 print(f"deposit:{deposit}")
 print(f"new balance after deposit:{new_balance}")
+
+withdraw=float(input("enter withdrawal amount"))
+new_balance-=withdraw
+print(f"withdraw",withdraw)
+print(f"new_balance",balance)
